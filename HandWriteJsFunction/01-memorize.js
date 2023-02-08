5e5c7ef8a139f2f5facb8a8cb279f7026fa79e5e
gongyuqi <gong_yu_qi@163.com>
2023-02-08 15:08:01 +0800

    memorize

--- HandWriteJsFunction/01-memorize.js
+++ HandWriteJsFunction/01-memorize.js
@@ -1,1 +1,24 @@
-console.log('hello')
+/**
+ * 缓存函数 能够将函数的计算结果进行缓存
+ * @param {*} fn 接收一个纯函数
+ * @returns 返回一个缓存函数 多次调用该函数并且参数相同 则只会调用原函数一次
+ */
+const memorize = function (fn) {
+  let cache = {}
+  return function (key) {
+    cache[key] = cache[key] || fn.apply(fn, arguments)
+    return cache[key]
+  }
+}
+
+const getArea = function (r) {
+  console.log(r)
+  return Math.PI * r * r
+}
+
+const getAreaWithMemory = memorize(getArea)
+
+// test
+console.log(getAreaWithMemory(4));
+console.log(getAreaWithMemory(4));
+console.log(getAreaWithMemory(4));
