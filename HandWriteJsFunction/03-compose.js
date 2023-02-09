cb08a2c41559d34c79328a13a27d1e83f675f7fc
gongyuqi <gong_yu_qi@163.com>
2023-02-09 18:03:50 +0800

    review

--- HandWriteJsFunction/03-compose.js
+++ HandWriteJsFunction/03-compose.js
@@ -1,3 +1,4 @@
+const m = require('./02-curry')
 // compose 组合函数 
 // 将接收的函数按从右到左的顺序执行 一个函数的返回值作为下一个函数的参数
 // es5
@@ -23,5 +24,19 @@ const toUpper = s => s.toUpperCase()
 
 const f = flowRight(toUpper, first, reverse)
 
-const arr = ['andy', 'bob', 'lynn']
-console.log(f(arr))
+// const arr = ['andy', 'bob', 'lynn']
+// console.log(f(arr))
+
+// 将 "NEVER SAY DIE" 转换成 "never-say-die"
+
+const split = m.curry((sep, str) => str.split(sep))
+
+const join = m.curry((sep, arr) => arr.join(sep))
+
+const toLower = s => s.toLowerCase()
+
+const map = m.curry((fn, arr) => arr.map(fn))
+
+const fn = compose(join('-'), map(toLower), split(' '))
+
+console.log(fn('NEVER SAY DIE'))
