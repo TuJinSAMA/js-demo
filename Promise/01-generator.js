6a3bd59ea3ccdbdce36f6c2464fec9e82d1e7f5a
gongyuqi <gong_yu_qi@163.com>
2023-02-16 14:22:04 +0800

    generator

--- Promise/01-generator.js
+++ Promise/01-generator.js
@@ -2,12 +2,23 @@ import axios from "axios";
 
 function* gen() {
   const { data } = yield axios.get("/json/url.json")
-  console.log(data.user)
+  console.log(data)
+  const result = yield axios.get(data.user)
+  console.log(result.data)
 
 }
 
-let g = gen()
+function run(generator) {
+  const _g = generator()
 
-g.next().value.then(value => {
-  g.next(value)
-})
+  function next(data) {
+    let _result = _g.next(data)
+    if (_result.done) return _result.value
+    _result.value.then(res => {
+      return next(res)
+    })
+  }
+  return next()
+}
+
+run(gen)
